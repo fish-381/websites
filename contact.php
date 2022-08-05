91ce73040570a341636775d29c19fef0acfa8fda
fish_381 <62921080+fish-381@users.noreply.github.com>
2022-08-05 11:52:59 +1000

    wright to file

--- contact.php
+++ contact.php
@@ -5,6 +5,7 @@ $email = $_POST['email']
 $body = $_POST['messagebo']
 $phnum = $_POST['phonenum']
 
-
-
+$fp = fopen('data.txt', 'a')
+fwrite($fp, `name: $name \n email $email \n body $body \n phone number $phnum `)
+fclose($fp)
 ?>
